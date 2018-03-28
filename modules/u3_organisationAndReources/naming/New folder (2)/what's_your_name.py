38918d6440ba2f74cce8f13d2c126df9bf240d44
Lucy Rachman Vascotto <rac0003@northcote-hs.edu.vic.gov.au>
2018-03-29 10:20:31 +1100

    Renamed variables

---
+++ modules/u3_organisationAndReources/naming/New folder (2)/what's_your_name.py
@@ -0,0 +1,9 @@
+"""
+Prog:   what's your name?
+Name:   Student Name
+Date:   12/03/18
+Desc:   Says hello to the user.
+"""
+
+name = input('What is your name? ')
+print('Hello ' + name + ', I am Computer! Nice to meet you.')

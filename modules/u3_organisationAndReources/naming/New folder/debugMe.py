563721624025b8cb17eb091db49c2791e6a854af
Lucy Rachman Vascotto <rac0003@northcote-hs.edu.vic.gov.au>
2018-03-29 10:18:33 +1100

    debuged the code

--- modules/u3_organisationAndReources/naming/New folder/debugMe.py
+++ modules/u3_organisationAndReources/naming/New folder/debugMe.py
@@ -4,21 +4,21 @@ Name:   Student Name
 Date:   12/03/18
 Desc:   Answers yes or no to any question.
 """
+import random
 
 # Ask the user for a question.
-question = print('Ask me anything!')
+question = input('Ask me anything!')
 
 # Check for special input.
-if  question = 'Quit'
-    print('Goodbye.)
-else if question == 'Hi' or 'Hello':
-    print('What's up?')
-
+if  question == 'Quit':
+    print('Goodbye.')
+elif question == 'Hi' or 'Hello':
+    print("What's up?")
 # Answer yes or no randomly.
 else:
 num = random.randint(0,4);
-if num == "1":
+if num == 1:
     print("Yes!")
-if num = 2:
-    "No"
+if num == 2:
+    print("No")
 done
